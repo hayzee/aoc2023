935bc93fae1d9a96147f8103a2adc1dc356d99d2
hayzee <hayzee@users.noreply.github.com>
2024-01-01 22:14:35 +0000

    Day 3 is a pain!

--- src/aoc2023/day03/day03.clj
+++ src/aoc2023/day03/day03.clj
@@ -14,7 +14,9 @@
   [file-data]
   (count (first file-data)))
 
-; (width file-data)
+(comment
+  (width file-data)
+  )
 
 (defn coords
   [file-data]
@@ -22,47 +24,49 @@
         c (range (width file-data))]
     [r c]))
 
-(defn file-data->map
-  [file-data]
-  (map
-   (fn [coord char] {:coords [coord] :chars [char]})
-   (coords file-data)
-   (apply str file-data)))
-
-(def file-data-map (file-data->map file-data))
-
 (defn digit?
   [ch]
   (boolean (#{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9} ch)))
 
+(defn space?
+  [ch]
+  (= \. ch))
+
 (defn sym?
   [ch]
   (not (or (digit? ch)
-           (= \. ch))))
+           (space? ch))))
 
-(defn extract-numbers
+(defn file-data->map
+  [file-data]
+  (map
+    (fn [coord char] {:coord coord :char char :type (cond
+                                                      (digit? char) :digit
+                                                      (sym? char) :sym
+                                                      (space? char) :space)})
+    (coords file-data)
+    (apply str file-data)))
+
+(def file-data-map (file-data->map file-data))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn tokens
+  [line]
+  (hash-map
+    :coords (set (map :coord line))
+    :type (first (map :type line))
+    :token (str (reduce str (map :char line)))))
+
+;(tokens [{:coord [9 5], :char \5} {:coord [9 6], :char \9} {:coord [9 7], :char \8}])
+
+(defn tokenise
   [file-data-map]
-  (->> (partition-by (fn [me] (digit? (first (:chars me)))) file-data-map)
-       (filter (fn [maps] (digit? (first (:chars (first maps))))))
-       (map (fn [maps] (-> (apply merge-with concat maps)
-                           (update :chars #(Integer/parseInt (apply str %)))
-                           (st/rename-keys {:chars :num})
-                           (update :coords set))))))
+  (->> (partition-by :type file-data-map)
+       (map tokens)))
 
-(extract-numbers file-data-map)
-
-(defn extract-symbols
-  [file-data-map]
-  (->> (partition-by (fn [me] (sym? (first (:chars me)))) file-data-map)
-       (filter (fn [maps] (sym? (first (:chars (first maps))))))
-       (map (fn [maps] (-> (apply merge-with concat maps)
-                           ;(update :chars #(Integer/parseInt (apply str %)))
-                           (st/rename-keys {:chars :num})
-                           (update :coords set))))))
-
-(extract-symbols file-data-map)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(->> (tokenise file-data-map)
+     (remove (fn [m] (= (:type m) :space))))
 
 (defn surrounds
   [[x y]]
@@ -70,36 +74,5 @@
    [(dec x) y] [(inc x) y]
    [(dec x) (inc y)] [x (inc y)][(inc x) (inc y)] ])
 
-; test file
+(surrounds [8 5])
 
-(-> (slurp (io/resource "day03/test.txt"))
-    (s/replace "\n" "")
-    (s/replace "\r" "")
-    #_(->> (map-indexed (fn [i e] [i e]))
-         (partition-by (comp digit? second))))
-
-(defn coords
-  [width]
-  (for [x (range)
-        y (range width)]
-    [x y]))
-
-(defn read-file
-  [fname]
-  (-> (slurp (io/resource fname))
-      (s/replace "\n" "")
-      (s/replace "\r" "")))
-
-(defn reduce-thing
-  [thing]
-  {:coords (mapv first thing)
-   :num (map second thing)
-   :type (cond (#{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9} (second (first thing))) :number
-               :else :symbol)})
-
-(->> (read-file "day03/test.txt")
-     (mapv vector (coords 10))
-     ;(partition-by #(= \. (second %)))
-     ;(remove #(= (second (first %)) \.))
-     ;(map reduce-thing)
-     )
